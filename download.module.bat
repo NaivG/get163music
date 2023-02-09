83c6201edd132b8cb01a5b05fe53d75c690a5ecc
NaivG <69287057+NaivG@users.noreply.github.com>
2023-02-10 06:53:15 +0800

    update to b0.3.0
    add multithreads support (beta)

---
+++ download.module.bat
@@ -0,0 +1,40 @@
+@echo off
+echo ===========thread start===========
+
+if "%runmode%"=="artist" (
+    if exist "%cd%\music\%name% - %artist%.mp3" (
+        echo exist.skipping...
+        goto :skip
+    )
+) else (
+    if exist "%cd%\music\%name%.mp3" (
+        echo exist.skipping...
+        goto :skip
+    )
+)
+if exist "%cd%\backup\%name%.mp3" (
+    echo exist.skipping...
+    goto :skip
+)
+
+if "%runmode%"=="artist" (
+    aria2c.exe --file-allocation=none -V --max-connection-per-server=16 --min-split-size=1M --dir="music" --out="%name% - %artist%.mp3" http://music.163.com/song/media/outer/url?id=%id%.mp3
+) else (
+    aria2c.exe --file-allocation=none -V --max-connection-per-server=16 --min-split-size=1M --dir="music" --out="%name%.mp3" http://music.163.com/song/media/outer/url?id=%id%.mp3
+)
+
+if "%dlyric%"=="false" goto :skip
+set lyric_url=http://music.163.com/api/song/media?id=%id%
+if "%runmode%"=="artist" (
+    set "lyric_name=%name% - %artist%.lrc"
+) else (
+    set "lyric_name=%name%.lrc"
+)
+if exist "%cd%\music\%name%.lrc" (
+    echo exist.skipping...
+    goto :skip
+)
+python getlyric.py
+
+:skip
+echo ===========thread end===========
--- main.bat
+++ main.bat
@@ -1,11 +1,17 @@
 @echo off
 md music
-set version=b0.2.8
+set version=b0.3.0
 set runmode=normal
 set dlyric=false
+set downmode=false
 cls
 echo running in normal mode
 echo type "switchtoartist" to switch artist mode
+echo.
+echo since b0.3.0 update,download music can be in multithreads.
+echo this default is false.
+echo type "multithreads" to switch it.
+echo.
 echo since b0.2.8 update,you can use pullplaylist beta.
 echo it will return music name including artist.
 echo.
@@ -15,6 +21,15 @@ echo type "nolyric" or "downloadlyric" to switch it.
 title Download Music %version%
 set /p id=:
 
+if "%id%"=="multithreads" (
+    echo switched.
+    if "%downmode%"=="false" (
+        set downmode=true
+    ) else (
+        set downmode=false
+    )
+    goto :idd
+)
 if "%id%"=="switchtoartist" (
     echo switched.
     set runmode=artist
@@ -41,46 +56,52 @@ if "%runmode%"=="artist" (
 ) else (
     title Download Music %version%-Downloading %id%
 )
-if "%runmode%"=="artist" (
-    if exist "%cd%\music\%name% - %artist%.mp3" (
-        echo exist.skipping...
-        goto :idd
-    )
-) else (
-    if exist "%cd%\music\%name%.mp3" (
-        echo exist.skipping...
-        goto :idd
-    )
-)
-if exist "%cd%\backup\%name%.mp3" (
-    echo exist.skipping...
-    goto :idd
-)
+rem if "%runmode%"=="artist" (
+rem     if exist "%cd%\music\%name% - %artist%.mp3" (
+rem         echo exist.skipping...
+rem         goto :idd
+rem     )
+rem ) else (
+rem     if exist "%cd%\music\%name%.mp3" (
+rem         echo exist.skipping...
+rem         goto :idd
+rem     )
+rem )
+rem if exist "%cd%\backup\%name%.mp3" (
+rem     echo exist.skipping...
+rem     goto :idd
+rem )
 
 echo =============ÏÂÔØÖÐ=============
-if "%runmode%"=="artist" (
-    aria2c.exe --file-allocation=none -V --max-connection-per-server=16 --min-split-size=1M --dir="music" --out="%name% - %artist%.mp3" http://music.163.com/song/media/outer/url?id=%id%.mp3
-) else (
-    aria2c.exe --file-allocation=none -V --max-connection-per-server=16 --min-split-size=1M --dir="music" --out="%name%.mp3" http://music.163.com/song/media/outer/url?id=%id%.mp3
-)
+rem if "%runmode%"=="artist" (
+rem     aria2c.exe --file-allocation=none -V --max-connection-per-server=16 --min-split-size=1M --dir="music" --out="%name% - %artist%.mp3" http://music.163.com/song/media/outer/url?id=%id%.mp3
+rem ) else (
+rem     aria2c.exe --file-allocation=none -V --max-connection-per-server=16 --min-split-size=1M --dir="music" --out="%name%.mp3" http://music.163.com/song/media/outer/url?id=%id%.mp3
+rem )
+
 ::wget http://music.163.com/song/media/outer/url?id=%id%.mp3 --output-file="%name%.mp3"
 ::title Download Music 0.1-Downloading %id%
 ::copy *.mp3 %cd%\music
 ::del /q "%name%.mp3"
 
-if "%dlyric%"=="false" goto :idd
-set lyric_url=http://music.163.com/api/song/media?id=%id%
-if "%runmode%"=="artist" (
-    set "lyric_name=%name% - %artist%.lrc"
+rem if "%dlyric%"=="false" goto :idd
+rem set lyric_url=http://music.163.com/api/song/media?id=%id%
+rem if "%runmode%"=="artist" (
+rem     set "lyric_name=%name% - %artist%.lrc"
+rem ) else (
+rem     set "lyric_name=%name%.lrc"
+rem )
+rem if exist "%cd%\music\%name%.lrc" (
+rem     echo exist.skipping...
+rem     goto :idd
+rem )
+rem python getlyric.py
+if "%downmode%"=="false" (
+    call download.module
 ) else (
-    set "lyric_name=%name%.lrc"
+    start multithreads %id% "%name%" %dlyric% %runmode% %artist%
+    ping -n 2 127.1>nul
 )
-if exist "%cd%\music\%name%.lrc" (
-    echo exist.skipping...
-    goto :idd
-)
-python getlyric.py
-
 goto idd
 
 :setupartist
