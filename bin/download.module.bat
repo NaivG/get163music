8bf65b6b7a82650c5dc79477f3cd8230e9888fdc
NaivG <69287057+NaivG@users.noreply.github.com>
2023-02-14 20:58:22 +0800

    add auto delbrokenfile support

--- bin/download.module.bat
+++ bin/download.module.bat
@@ -23,6 +23,24 @@ if "%runmode%"=="artist" (
     bin\aria2c.exe --file-allocation=none -V --max-connection-per-server=16 --min-split-size=1M --dir="music" --out="%name%.mp3" http://music.163.com/song/media/outer/url?id=%id%.mp3
 )
 
+if "%runmode%"=="artist" (
+    set "file=%cd%\music\%name% - %artist%.mp3"
+) else (
+    set "file=%cd%\music\%name%.mp3"
+)
+if not exist "%file%" goto :checkskip
+setlocal ENABLEDELAYEDEXPANSION
+for %%i in ("%file%") do (
+   set size=%%~zi
+if !size! leq 204800 (
+      echo 下载错误：%%i 大小为 !kb! KB 不满足大于200KB
+      md .\music\BROKENFILE
+      move "%file%" .\music\BROKENFILE
+   )
+)
+endlocal
+:checkskip
+
 if "%dlyric%"=="false" goto :skip
 set lyric_url=http://music.163.com/api/song/media?id=%id%
 if "%runmode%"=="artist" (
